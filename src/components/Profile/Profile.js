a975d56baefded601a453aeb4c10cd3aca33e4a9
Nikolay <103648794+gilotin@users.noreply.github.com>
2023-03-25 17:42:44 +0200

    added basic profile structure

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,12 +1,28 @@
 // import { Link } from "react-router-dom";
-import { Button } from "react-bootstrap";
+import { Button, Card } from "react-bootstrap";
 
 export function Profile() {
     return (
         <>
             <h1>Place for your profile</h1>
             {/* <Link to={`/profile/create`}>Create</Link> */}
-            <Button type="button" href="/profile/create">Create </Button>{' '}
-            </>
+            <Card>
+                <img src="" alt="Profile Pic" />
+            </Card>
+            <h3>Hello Username!!!</h3>
+            <h4></h4>
+            <Button type="button" href="/profile/create">
+                Create{" "}
+            </Button>{" "}
+            <Button type="button" href="/profile/deleteProfile">
+                Delete Profile{" "}
+            </Button>{" "}
+            <Button type="button" href="/profile/myCollection">
+                My Collection{" "}
+            </Button>{" "}
+            <Button type="button" href="/profile/editProfile">
+                Edit Profile
+            </Button>{" "}
+        </>
     );
 }

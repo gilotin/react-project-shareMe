b545aa363195e3e9b6acb7eac93dc08d1e0466c8
Nikolay <103648794+gilotin@users.noreply.github.com>
2023-03-27 22:37:44 +0300

    added modified server for the project

---
+++ server/data.js
@@ -0,0 +1,95 @@
+var seedData = {
+    catalog: {
+        "a0f4f7fd-4642-4fc8-93b3-a0db34b63f76": {
+            "url": "https://cdn.aarp.net/content/dam/aarp/travel/Domestic/2020/07/1140-rocky-mountain-national-park.jpg",
+            "title": "Rock Mountain",
+            "author": "Freysteinn G. Jonsson",
+            "location": {
+                "country": "USA",
+                "city": "California"
+            },
+            "description": "One day near California.",
+            "_id": "a0f4f7fd-4642-4fc8-93b3-a0db34b63f76"
+        },
+        "914ee086-36cf-4eb1-af48-14f0f62746fe": {
+            "url": "https://hellobark.com/wp-content/uploads/good-boy-ollie-5-1024x683.jpg",
+            "title": "Good boy",
+            "author": "Hanson Lu",
+            "location": {
+                "country": "USA",
+                "city": "Washington DC"
+            },
+            "description": "Happy doggo!",
+            "_id": "914ee086-36cf-4eb1-af48-14f0f62746fe"
+        },
+        "8fffbd77-1f77-423c-a935-2b9213b0481d": {
+            "url": "https://images.immediate.co.uk/production/volatile/sites/23/2022/05/Slow-loris-b5f7ac4.jpg",
+            "title": "What's there ?",
+            "author": "Angus Gray",
+            "location": {
+                "country": "Brasil",
+                "city": "São Paulo"
+            },
+            "description": "Captured in the local ZOO.",
+            "_id": "8fffbd77-1f77-423c-a935-2b9213b0481d"
+        },
+        "40df044d-6e83-406c-8338-de6e90967bbb": {
+            "url": "https://www.johnmaxwell.com/wp-content/uploads/2019/12/faye-cornish-Uq3gTiPlqRo-unsplash.jpg",
+            "title": "What's there ?",
+            "author": "Logan Weaver",
+            "location": {
+                "country": "USA",
+                "city": "Boulder, CO"
+            },
+            "description": "Me, myself and Aireen",
+            "_id": "40df044d-6e83-406c-8338-de6e90967bbb"
+        },
+        "468152db-39bb-49ab-ba8f-1744dda1ccc9": {
+            "url": "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F44%2F2021%2F12%2F02%2Fhow-to-help-your-kids-eat-healthy-without-calling-foods-good-and-bad.jpg&q=60",
+            "title": "Yummy",
+            "author": "Christian Dala",
+            "location": {
+                "country": "France",
+                "city": "Paris"
+            },
+            "description": "That was the best cake in my life.",
+            "_id": "468152db-39bb-49ab-ba8f-1744dda1ccc9"
+        },
+        "dbe1a502-f7d3-4d83-9c63-9d5db6760cd9": {
+            "url": "https://thebakingacademy.idltickets.com/v1/coursepix/1560268237.jpg",
+            "title": "The art of cooking.",
+            "author": "Mattia Occhi",
+            "location": {
+                "country": "Italy",
+                "city": "Ferrara"
+            },
+            "description": "This is the chef of the local restaurant, preparing chocolate.",
+            "_id": "dbe1a502-f7d3-4d83-9c63-9d5db6760cd9"
+        },
+        "5ed134c2-18ef-4f23-9094-4f8bd095fff4": {
+            "url": "http://onebigphoto.com/uploads/2013/09/good-morning-paris.jpg",
+            "title": "Good morning Paris.",
+            "author": "Timelab",
+            "location": {
+                "country": "France",
+                "city": "Paris"
+            },
+            "description": "Drone aerial close up shot of Eiffel Tower, Paris, France. Filmed in 6K DNG RAW on DJI Inspire 2. Make the same color on your photo: https://luts.timelab.pro/catalog",
+            "_id": "5ed134c2-18ef-4f23-9094-4f8bd095fff4"
+        },
+        "48c81435-ed52-4b1d-92ac-297b1eb7aff3": {
+            "url": "https://cdn.mos.cms.futurecdn.net/YMa7Wx2FyjQFUjEeqa72Rm.jpg",
+            "title": "The pyramids of Giza",
+            "author": "2H Media",
+            "location": {
+                "country": "Egypt",
+                "city": "Al Haram"
+            },
+            "description": "The pyramids of Giza",
+            "_id": "48c81435-ed52-4b1d-92ac-297b1eb7aff3"
+        }
+    },
+    comments: {
+    
+    }
+};
